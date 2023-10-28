0d4811279599585a6ae26fb470e10017d9c5d1bb
Sandro Verdin <s.verdin@student.helmo.be>
2023-10-29 00:01:03 +0200

    change to war

---
+++ src/main/java/org/helmo/HolyD/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.helmo.HolyD;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HolyDApplication.class);
+	}
+
+}
