83b8bae277f827208508c973d5cfabdf0f23812e
Sandro Verdin <s.verdin@student.helmo.be>
2023-10-28 22:55:36 +0200

    mapper

--- src/main/java/org/helmo/HolyD/controlers/UserControler.java
+++ src/main/java/org/helmo/HolyD/controlers/UserControler.java
@@ -1,10 +1,15 @@
 package org.helmo.HolyD.controlers;
 
 import org.helmo.HolyD.controlers.swagger.UserControlerSwagger;
+import org.helmo.HolyD.models.requests.UserSignIn;
+import org.helmo.HolyD.models.requests.UserSignUp;
 import org.helmo.HolyD.repository.DTO.UserDTO;
 import org.helmo.HolyD.repository.UserRepository;
 import org.helmo.HolyD.repository.exception.UserAlreadyExistException;
 import org.helmo.HolyD.repository.exception.UserNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,24 +19,27 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserControler implements UserControlerSwagger {
 
+    private final ModelMapper modelMapper;
     private final UserRepository repository;
 
 
-    public UserControler(UserRepository repository) {
+    public UserControler(UserRepository repository, ModelMapper modelMapper) {
         this.repository = repository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     @PutMapping("/signup")
-    public UserDTO putUser(@Valid @RequestBody UserDTO user){
+    public ResponseEntity<UserDTO> putUser(@Valid @RequestBody UserSignUp user){
         if (repository.existsByEmail(user.getEmail())){
             throw new UserAlreadyExistException();
         }
-        return repository.saveAndFlush(user);
+
+        return new ResponseEntity<UserDTO>(repository.saveAndFlush(modelMapper.map(user, UserDTO.class)), HttpStatus.OK);
     }
     @Override
     @PostMapping("/signin")
-    public UserDTO postUser(@Valid @RequestBody UserDTO user){
+    public UserDTO postUser(@Valid @RequestBody UserSignIn user){
         return repository.findByEmailAndPasswd(user.getEmail(), user.getPasswd())
                 .orElseThrow(UserNotFoundException::new);
     }
--- src/main/java/org/helmo/HolyD/controlers/swagger/UserControlerSwagger.java
+++ src/main/java/org/helmo/HolyD/controlers/swagger/UserControlerSwagger.java
@@ -7,17 +7,20 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
+import org.helmo.HolyD.models.requests.UserSignIn;
+import org.helmo.HolyD.models.requests.UserSignUp;
 import org.helmo.HolyD.repository.DTO.UserDTO;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 
 @Api(tags = "user")
 public interface UserControlerSwagger {
 
     @Operation(operationId = "UserControler", summary = "Used to sign up a user.", description = "Return a user")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDTO.class)))
-    UserDTO putUser(@RequestBody UserDTO user);
+    ResponseEntity<UserDTO> putUser(@RequestBody UserSignUp user);
 
     @Operation(operationId = "UserControler", summary = "Used to sign in a user.", description = "Return a user")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDTO.class)))
-    UserDTO postUser(@RequestBody UserDTO user);
+    UserDTO postUser(@RequestBody UserSignIn user);
 }
